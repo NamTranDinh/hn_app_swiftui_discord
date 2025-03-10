bd3dc54e50806a7b89da655c4a9a0fb09ff78df0
tdnam3 <tdnam3@cmcglobal.vn>
2025-03-10 09:05:43 +0700

    init: commit

---
+++ DiscordApp/DiscordApp/DiscordAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordAppApp.swift
+//  DiscordApp
+//
+//  Created by Nam. Tran Dinh - CMC Global DKR1 on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DiscordAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
